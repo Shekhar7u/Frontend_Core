ec5b0580037c8f890a12d08648866572b3d514d6
shekhar <singhskr7@gmail.com>
2023-09-03 23:29:18 +0530

    ok

--- News Website/index.js
+++ News Website/index.js
@@ -1,23 +1,18 @@
-const API_KEY="b2361ebafb984c2ba873bb06bd53177c"
-const url="https://newsapi.org/v2/everything?q=";
+const API_KEY = "b2361ebafb984c2ba873bb06bd53177c"
+const url = "https://newsapi.org/v2/everything?q=";
 window.addEventListener("load", () => fetchNews("India"));
-
 function reload() {
     window.location.reload();
 }
-
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
     bindData(data.articles);
 }
-
 function bindData(articles) {
     const cardsContainer = document.getElementById("cards-container");
     const newsCardTemplate = document.getElementById("template-news-card");
-
     cardsContainer.innerHTML = "";
-
     articles.forEach((article) => {
         if (!article.urlToImage) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
@@ -25,28 +20,23 @@ function bindData(articles) {
         cardsContainer.appendChild(cardClone);
     });
 }
-
 function fillDataInCard(cardClone, article) {
     const newsImg = cardClone.querySelector("#news-img");
     const newsTitle = cardClone.querySelector("#news-title");
     const newsSource = cardClone.querySelector("#news-source");
     const newsDesc = cardClone.querySelector("#news-desc");
-
     newsImg.src = article.urlToImage;
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
-
     const date = new Date(article.publishedAt).toLocaleString("en-US", {
         timeZone: "Asia/Jakarta",
     });
-
     newsSource.innerHTML = `${article.source.name} · ${date}`;
 
     cardClone.firstElementChild.addEventListener("click", () => {
         window.open(article.url, "_blank");
     });
 }
-
 let curSelectedNav = null;
 function onNavItemClick(id) {
     fetchNews(id);
@@ -55,10 +45,8 @@ function onNavItemClick(id) {
     curSelectedNav = navItem;
     curSelectedNav.classList.add("active");
 }
-
 const searchButton = document.getElementById("search-button");
 const searchText = document.getElementById("search-text");
-
 searchButton.addEventListener("click", () => {
     const query = searchText.value;
     if (!query) return;
